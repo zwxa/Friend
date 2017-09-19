4c333266a71c07c454ed7f92322721c12fc8ed39
zhangwenxuan <593693444@qq.com>
2017-09-19 09:58:46 +0800

    login-page

---
+++ src/components/login.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="login">
+  	<div class="box">
+  		<h1>叫啥玩意？</h1>
+		<input type="text" v-model.trim="userid" @keyup.enter="login">
+	</div>
+  </div>
+</template>
+
+<script>
+import Vidage from 'Vidage';
+
+export default {
+	data () {
+		return {
+			userid:''
+		}
+	},
+	created(){
+		
+	},
+	methods:{
+		login(){
+			this.$router.push({path:'Hello',query:{userid:this.userid}});
+		}
+	}
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+}
+.login{
+position: fixed;
+top:0;
+bottom: 0;
+left: 0;
+right: 0;
+background-color: #000;
+opacity: .8;
+}
+.login .box{
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%,-50%);
+	color:#fff;
+}
+.box input{
+	height: 24px;
+	line-height: 24px;
+	border-radius: 4px;
+	margin-top: 6px;
+	padding: 0 10px;
+}
+</style>
